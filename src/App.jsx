a5e519dbfcff22ede03021aa5defa203b5dd27b9
vichumsv <vichu.engg@gmail.com>
2025-06-14 16:50:17 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <>
+      <h1 className="">Helloo</h1>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import Logo from "./images/logo.png";
+const Header = () => {
+  return (
+    <div>
+      <img src={Logo} alt="Logo" />
+    </div>
+  );
+};
+
+export default Header;
